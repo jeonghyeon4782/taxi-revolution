762d7ee742e978488a28523f3f899266f3fc2e3b
wjd47 <wjd4782@naver.com>
2023-08-01 17:13:02 +0900

    2023-08-01

--- app/src/main/java/com/jeonghyeon/taxiproject/fragment/NoticeBoardFragment.java
+++ app/src/main/java/com/jeonghyeon/taxiproject/fragment/NoticeBoardFragment.java
@@ -221,7 +221,7 @@ public class NoticeBoardFragment extends Fragment {
         String accessToken = tokenManager.getAccessToken();
             // Retrofit 객체 생성
             Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("http://121.200.87.205:8000/") // 스프링부트 API의 기본 URL을 설정
+                    .baseUrl("http://58.121.164.22:8000/") // 스프링부트 API의 기본 URL을 설정
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
 
