6622ef0d506ae8e50004a08c3e4c431cf4838b9b
wjd47 <wjd4782@naver.com>
2023-09-12 21:28:25 +0900

    2023-09-12

--- app/src/main/java/com/jeonghyeon/taxiproject/adapter/PostAdapter.java
+++ app/src/main/java/com/jeonghyeon/taxiproject/adapter/PostAdapter.java
@@ -131,10 +131,11 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.RecordViewHold
             } else {
                 genderImageView.setImageResource(R.drawable.baseline_face_6_24);
             }
+
             // SimpleDateFormat을 사용하여 날짜 및 시간을 원하는 형식으로 포맷
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 HH시mm분", Locale.getDefault());
             try {
-                Date departureTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(post.getCreateTime());
+                Date departureTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(post.getDepartureTime());
                 departureTimeTextView.setText(dateFormat.format(departureTime));
             } catch (ParseException e) {
                 e.printStackTrace();
