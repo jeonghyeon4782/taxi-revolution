bdf023d2c883aeb3341d43ad2a133b8704372387
wjd47 <wjd4782@naver.com>
2023-10-15 16:59:17 +0900

    2023-10-15

--- src/main/java/com/wjd4782/taxiprojectrestapi/dto/request/MemberLoginRequestDto.java
+++ src/main/java/com/wjd4782/taxiprojectrestapi/dto/request/MemberLoginRequestDto.java
@@ -1,7 +1,5 @@
 package com.wjd4782.taxiprojectrestapi.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/com/wjd4782/taxiprojectrestapi/dto/request/MemberRegisterRequestDto.java
+++ src/main/java/com/wjd4782/taxiprojectrestapi/dto/request/MemberRegisterRequestDto.java
@@ -1,6 +1,5 @@
 package com.wjd4782.taxiprojectrestapi.dto.request;
 
-import com.wjd4782.taxiprojectrestapi.domain.Member;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,14 +10,4 @@ public class MemberRegisterRequestDto {
     private String password;
     private String nickname;
     private int gender; // 남자 : 0, 여자 : 1
-
-    // dto >> domain
-    public Member toEntity() {
-        return Member.builder()
-                .memberId(memberId)
-                .password(password)
-                .nickname(nickname)
-                .gender(gender)
-                .build();
-    }
 }
